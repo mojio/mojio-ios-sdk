e15970da21133aa11af4401d0d613e8fa8f61fc7
Vinson Li <vinson@twotalltotems.com>
2015-01-27 12:10:50 -0800

    Merged two projects into one with two targets

---
+++ MojioSDKiOS/MojioSDK-TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MojioSDK-TestApp
+//
+//  Created by Vinson Li on 2015-01-27.
+//  Copyright (c) 2015 Mojio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MojioSDKiOS/MojioSDK-TestAppTests/MojioSDK_TestAppTests.m
@@ -0,0 +1,40 @@
+//
+//  MojioSDK_TestAppTests.m
+//  MojioSDK-TestAppTests
+//
+//  Created by Vinson Li on 2015-01-27.
+//  Copyright (c) 2015 Mojio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MojioSDK_TestAppTests : XCTestCase
+
+@end
+
+@implementation MojioSDK_TestAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
