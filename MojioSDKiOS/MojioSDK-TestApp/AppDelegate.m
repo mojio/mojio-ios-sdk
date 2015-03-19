2620b86d310bb466d06966ad4d9b6d5b6d9061df
Ming-Tang <project.shinkirou@gmail.com>
2015-03-18 20:46:10 -0700

    Hide API keys

--- MojioSDKiOS/MojioSDK-TestApp/AppDelegate.m
+++ MojioSDKiOS/MojioSDK-TestApp/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     self.client = [MojioClient client];
-    [self.client initWithAppId:@"f642dfb4-67c2-472a-bec6-a583d3152a01" andSecretKey:@"6ab44a7f-d47b-4931-91fb-71905173c552" andRedirectUrlScheme:@"mojioios://"];
+    [self.client initWithAppId:@"<app ID>" andSecretKey:@"<secret key>" andRedirectUrlScheme:@"<appname>://"];
     
     return YES;
 }
