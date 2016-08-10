c52b305ec1e19f58bf5588689331231db8076146
Ashish Agarwal <iamashish.agarwal@gmail.com>
2016-08-10 14:34:05 -0700

    Added a ErrorType for display purposes

--- MojioSDK/Models/Error.swift
+++ MojioSDK/Models/Error.swift
@@ -18,3 +18,15 @@ public struct MojioError : ErrorType {
         self.dictionary = dictionary
     }
 }
+
+public struct AuthDisplayError : ErrorType {
+    public var message1 : String?
+    public var message2 : String?
+    public var message3 : String?
+
+    public init(message1 : String?, message2 : String? = nil, message3 : String? = nil) {
+        self.message1 = message1
+        self.message2 = message2
+        self.message3 = message3
+    }
+}
