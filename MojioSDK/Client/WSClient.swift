59333e2b5458055f122bd52929737f65b437531d
Suresh Venkatraman <sureshv@moj.io>
2016-12-05 14:31:31 -0800

    Allow for custom dispatch queue for websocket responses

--- MojioSDK/Client/WSClient.swift
+++ MojioSDK/Client/WSClient.swift
@@ -13,16 +13,24 @@ import SwiftyJSON
 
 public class WSClient : RestClient {
     
+    // Default to global concurrent queue with default priority
+    public static var wsDefaultDispatchQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+    private var wsDispatchQueue = WSClient.wsDefaultDispatchQueue
+    
+    public override func dispatch(queue: dispatch_queue_t) {
+        self.wsDispatchQueue = queue
+    }
+    
     public func watch(next: ((AnyObject) -> Void), completion: (() -> Void), failure: ((ErrorType) -> Void), file: String = #file) -> WebSocket {
-        
         
         let request = NSMutableURLRequest(URL: NSURL(string:super.pushUrl!)!)
         if let accessToken : String = super.accessToken() {
             request.addValue("Bearer " + accessToken, forHTTPHeaderField: "Authorization")
-
         }
         
         let ws = WebSocket(request: request)
+        
+        ws.eventQueue = self.wsDispatchQueue
         
         ws.event.close = { code, reason, clean in
             print("WEBSOCKET: CLOSED - \(file)")
