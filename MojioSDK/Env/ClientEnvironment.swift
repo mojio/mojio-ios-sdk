ad387829731983c490cf4ee6bda0ccb3670e31f8
Suresh Venkatraman <sureshv@moj.io>
2017-09-19 16:22:42 -0700

    NA stagingslot test release

--- MojioSDK/Env/ClientEnvironment.swift
+++ MojioSDK/Env/ClientEnvironment.swift
@@ -26,12 +26,12 @@ public class MojioRegion : NSObject {
 
 public class ClientEnvironment : NSObject {
     
-    private static let ApiEndpointFormat : String = "https://%@api.moj.io/v2/"
-    private static let ApiV1EndpointFormat : String = "https://%@api.moj.io/v1/"
-    private static let PushApnsEndpointFormat : String = "https://%@push.moj.io/v2/"
-    private static let PushWSEndpointFormat : String = "wss://%@api.moj.io/v2/"
+    private static let ApiEndpointFormat : String = "https://%@na-api-stagingslot.moj.io/v2/"
+    private static let ApiV1EndpointFormat : String = "https://%@na-api-stagingslot.moj.io/v1/"
+    private static let PushApnsEndpointFormat : String = "https://%@na-push-stagingslot.moj.io/v2/"
+    private static let PushWSEndpointFormat : String = "wss://%@na-api-stagingslot.moj.io/v2/"
     private static let MyMojioEndpointFormat : String = "https://%@my.moj.io/"
-    private static let AccountsEndpointFormat : String = "https://%@accounts.moj.io/"
+    private static let AccountsEndpointFormat : String = "https://%@identity-stagingslot.moj.io/"
     
     public static let SharedInstance = ClientEnvironment()
     
