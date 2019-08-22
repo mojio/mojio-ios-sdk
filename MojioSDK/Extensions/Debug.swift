567cfe6085a4e4db8a8dfb2675ad4478cadbff25
Oleg Yankiwskyi <Oleg_Yankiwskyi@epam.com>
2019-08-22 11:07:03 +0300

    Override print and debugPrint to work only in debug mode.

---
+++ MojioSDK/Extensions/Debug.swift
@@ -0,0 +1,28 @@
+/******************************************************************************
+ * Moj.io Inc. CONFIDENTIAL
+ * 2019 Copyright Moj.io Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains, the property of
+ * Moj.io Inc. and its suppliers, if any.  The intellectual and technical
+ * concepts contained herein are proprietary to Moj.io Inc. and its suppliers
+ * and may be covered by Patents, pending patents, and are protected by trade
+ * secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from Moj.io Inc.
+ *******************************************************************************/
+
+import Foundation
+
+public func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    #if DEBUG
+    Swift.print(items, separator: separator, terminator: terminator)
+    #endif
+}
+
+public func debugPrint(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    #if DEBUG
+    Swift.debugPrint(items, separator: separator, terminator: terminator)
+    #endif
+}
